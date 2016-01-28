63d0433606b05ec91467cca9bcab0c39ba993fe1
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-01-28 13:11:06 +1100

    4.0 HW with exception of randomcomputer calling tie

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,11 +21,12 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
+f
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-   (move === "rock" || move === "paper" || move === "scissors") || getInput();
+    (move === "rock" || move === "paper" || move === "scissors") || getInput();
     return move;
 }
 
@@ -35,16 +36,24 @@ function getComputerMove(move) {
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     (move === "rock" || move === "paper" || move === "scissors");
     return move || randomPlay();
-}   
+}
 
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+var winner;
+
+  if (playerMove === "rock" && computerMove === "scissors" || playerMove === "paper" && computerMove === "rock" || playerMove === "scissors" && computerMove === "paper") {
+   winner = "Player";
+ } else if 
+    (playerMove === "rock" && computerMove === "paper" || playerMove === "paper" && computerMove === "scissors" || playerMove === "scissors" && computerMove === "rock") { 
+    winner = "Computer";
+ } else {
+    (playerMove === computerMove ); 
+    winner = "tie";
 }
+return winner;
+}
+
+ 
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
